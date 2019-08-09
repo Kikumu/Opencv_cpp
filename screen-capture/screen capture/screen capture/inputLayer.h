31335b79a145f169705d70c7d842e421604ea307
_ten5ei <scowty.sk@gmail.com>
2019-08-09 16:34:15 +0100

    study

---
+++ screen-capture/screen capture/screen capture/inputLayer.h
@@ -0,0 +1,75 @@
+#pragma once
+#include "pch.h"
+#include <stdlib.h>
+#include <vector>
+#include "Neuron.h"
+#include "Layer.h"
+
+
+using namespace std;
+class inputLayer:public Layer //this is 'new'. review this later
+{
+public:
+	inputLayer();
+	~inputLayer();
+
+	inputLayer& initLayer(inputLayer& inputLayer);
+	void printLayer(inputLayer& inputLayer);
+private:
+
+};
+
+inputLayer::inputLayer()
+{
+}
+
+inputLayer::~inputLayer()
+{
+}
+
+inline inputLayer & inputLayer::initLayer(inputLayer & inputLayer)
+{
+	// TODO: insert return statement here
+	//each layer has neurons and weights connected to it hence:
+	vector<double>listOfWeightsInTemp;
+	vector<Neuron>pNeurons; 
+
+	//need to properly understand whats going on. why? because:
+	// the input layer can have a set number of neurons depending on how much input you wanna have
+	// the hidden layers, the one in between input and output neurons, you can decide how many neurons can be in each layer. its not determined by anything*
+	for (size_t i = 0; i < inputLayer.numberOfNeuronsInLayer; i++)
+	{
+		Neuron cNeuron; //create instance of neuron
+
+		listOfWeightsInTemp.push_back(cNeuron.initialiseNeuron()); //assigns weight to neuron randomly(see neuron.h)
+		cNeuron.weightsIn = listOfWeightsInTemp; //dont really know whats going on here. i understand but what i dont know is where are the weights initialised in neuron.h?
+		//well they are public so random values i guess? from main?
+		//yeah definitely from main. because the connections to layers may vary during training
+		pNeurons.push_back(cNeuron); //push back to vector the input vals
+		listOfWeightsInTemp.clear();
+	}
+	inputLayer.Neurons = pNeurons; //initialise gathered neurons from loop into the input layer
+	return inputLayer;
+}
+
+inline void inputLayer::printLayer(inputLayer & inputLayer)//pass by reference
+{
+	cout <<" INPUT LAYER ";
+
+	int nNeuron = 1;
+
+	//for each neuron in layer initialise weights
+	for (Neuron& neuron : inputLayer.Neurons)
+	{
+		cout << "Neuron: ";
+		cout << nNeuron;
+		cout << "\n";
+		cout << "input weights: ";
+		vector<double>weights = neuron.weightsIn;
+		for (double weight : weights)
+		{
+			cout << weight;
+		}
+		nNeuron++;
+	}
+}
