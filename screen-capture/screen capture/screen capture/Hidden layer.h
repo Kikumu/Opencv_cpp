0b1f433b0154068dea5b7270357eccb56b376a8b
_ten5ei <scowty.sk@gmail.com>
2019-08-09 16:59:09 +0100

    study

---
+++ screen-capture/screen capture/screen capture/Hidden layer.h
@@ -0,0 +1,6 @@
+#pragma once
+//needs to receive all data from previous layers
+//similar to input layer but different
+//functions are carried out here(activation functions, biases, drop outrates)
+//confidence rates, probabilities, decision making etc etc
+//'confidence scores' for which decision it decided to ultimately make
